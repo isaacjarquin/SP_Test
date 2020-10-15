40a594c484fdd3a789bdd2e34a6876535f200e7f
isaacjarquin <isaacjarquin@gmail.com>
2020-10-15 12:57:20 +0100

    add test to cover case when childre class has not implemented visited_pages method

--- spec/unit/presenters/output_presenter_spec.rb
+++ spec/unit/presenters/output_presenter_spec.rb
@@ -8,6 +8,14 @@ class ChildrenClass < LogParser::OutputPresenter
   end
 end
 
+class ChildrenWithNoVisitedPagesImplemented < LogParser::OutputPresenter
+  def present
+    output_sorted.map do |key, value|
+      "#{key} #{value} visits"
+    end
+  end
+end
+
 
 describe LogParser::OutputPresenter do
   let(:parsed_file) do
@@ -27,6 +35,18 @@ describe LogParser::OutputPresenter do
       expect {
         children_with_no_present_implemented.present
       }.to raise_error NoMethodError, 'Children classes need to implement the present method'
+    end
+  end
+
+  context 'Given children does not implement visit_pages method' do
+    let(:children_with_no_visited_pages_implemented) {
+      ChildrenWithNoVisitedPagesImplemented.new(parsed_file)
+    }
+
+    it 'raises a NoMethodError' do
+      expect {
+        children_with_no_visited_pages_implemented.present
+      }.to raise_error NoMethodError, 'Children classes need to implement the visited_pages method'
     end
   end
 end
